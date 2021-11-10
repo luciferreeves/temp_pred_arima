3199c6ffee887c1dd21fbaecf9fdfe8c8a25a4cf
Priyansh <bobbyskhs@gmail.com>
2021-11-10 00:52:06 -0500

    Added Image graph from Flask

--- dashboard/app.py
+++ dashboard/app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def index():
-	return render_template("index.html")
+    graph = 'https://humansofdata.atlan.com/wp-content/uploads/2016/11/Line-Graph.png'
+    return render_template("index.html", graph=graph)
 
 @app.route("/map")
 def map():
@@ -17,4 +18,4 @@ def map():
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
