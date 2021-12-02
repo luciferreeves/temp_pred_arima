74d6f019cc90214833c00ec45ed92ffc45c07dd7
Priyansh <bobbyskhs@gmail.com>
2021-12-02 16:37:41 -0500

    Isolated SQL Functions, Passed JS Data to Flask, time series in a separate function

--- app.py
+++ app.py
@@ -1,35 +1,34 @@
 from flask import Flask, render_template
-from flask import Markup
-import sqlite3 as sql
+from flask import request
 import pandas as pd
+from sql_functions import run_sql_pandas, get_list_of_dict
+from time_series_model import plot_data
 
 app = Flask(__name__)
 
-
-def execute_sql_statement(sql_statement, conn):
-    cur = conn.cursor()
-    cur.execute(sql_statement)
-    rows = cur.fetchall()
-    return rows
-
-def get_list_of_dict(keys, list_of_tuples):
-     list_of_dict = [dict(zip(keys, values)) for values in list_of_tuples]
-     return list_of_dict
-
 def get_cities_list():
-    conn = sql.connect("database.db")
     sql_statement = 'SELECT DISTINCT(City_Name) as City_Name, Lat, Long FROM City_table join Loc_Table on City_Table.City_Id = Loc_table.City_Id'
-    df=pd.read_sql_query(sql_statement, conn).to_records(index=False)
-    return df
+    return run_sql_pandas(sql_statement)
 
 @app.route("/")
 @app.route("/index")
 def index():
-
     cities_list = get_cities_list()
     keys = ("city", "latitude", "longitude")
     cities_list = get_list_of_dict(keys, cities_list)
     return render_template("index.html", cities_list=cities_list)
+
+
+@app.route("/receiveDates", methods=["POST"])
+def receive_dates():
+    start_date = request.form["start_date"]
+    end_date = request.form["end_date"]
+    dates_unmf = pd.date_range(start_date, end_date, freq='d')
+    dates = []
+    for i in dates_unmf:
+        dates.append(i.strftime('%Y-%m-%d'))
+    plot_data(dates)
+    return 'OK'
 
 if __name__ == '__main__':
     app.run(debug=True)
