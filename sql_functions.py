15b1a734b0b44bebe64df8f0bd514ac68dabb967
Priyansh <bobbyskhs@gmail.com>
2021-12-09 01:40:24 -0500

    Refactor code and time_series_model.py now builds a pickle file

--- sql_functions.py
+++ sql_functions.py
@@ -1,6 +1,7 @@
 import sqlite3 as sql
 import pandas as pd
 
+
 def execute_sql_statement(sql_statement):
     conn = sql.connect("database.db")
     cur = conn.cursor()
@@ -8,11 +9,13 @@ def execute_sql_statement(sql_statement):
     rows = cur.fetchall()
     return rows
 
+
 def run_sql_pandas(sql_statement):
     conn = sql.connect("database.db")
-    df=pd.read_sql_query(sql_statement, conn).to_records(index=False)
+    df = pd.read_sql_query(sql_statement, conn).to_records(index=False)
     return df
 
+
 def get_list_of_dict(keys, list_of_tuples):
-     list_of_dict = [dict(zip(keys, values)) for values in list_of_tuples]
-     return list_of_dict
+    list_of_dict = [dict(zip(keys, values)) for values in list_of_tuples]
+    return list_of_dict
