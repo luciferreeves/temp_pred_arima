22e1d761027501bfa59b92776cf1c13eef3a0004
Priyansh <bobbyskhs@gmail.com>
2021-12-09 02:50:26 -0500

    Changing compressor message

--- zipper.py
+++ zipper.py
@@ -9,6 +9,6 @@ if exists(filename_out):
     remove(filename_out)
 with open(filename_in, mode="rb") as fin, open(filename_out, mode="wb") as fout:
     data = fin.read()
-    print("Compressing Pickle File for Version Control...")
+    print("Compressing Pickle File...")
     compressed_data = zlib.compress(data, zlib.Z_BEST_COMPRESSION)
     fout.write(compressed_data)
